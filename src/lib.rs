d0c784da1c84105b8050e8486fb60145e49bbddd
Nathan F Yospe <nyospe@gmail.com>
2024-02-29 17:39:23 -0500

    adding static version

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,6 @@
+use versioned_binary_serialization::version::StaticVersion;
+
 pub const MAJOR: u16 = 0;
 pub const MINOR: u16 = 1;
+
+pub const SEQUENCER_VERSION: StaticVersion<MAJOR, MINOR> = StaticVersion;
