9fa034d3c1b2f9d576a78d44a9de6355e87aa559
Nathan F Yospe <nyospe@gmail.com>
2024-03-20 12:03:30 -0400

    Updating constants to include the type.

--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,6 @@ use versioned_binary_serialization::version::StaticVersion;
 pub const MAJOR: u16 = 0;
 pub const MINOR: u16 = 1;
 
-pub const SEQUENCER_VERSION: StaticVersion<MAJOR, MINOR> = StaticVersion;
+pub type SequencerVersion = StaticVersion<MAJOR, MINOR>;
+
+pub const SEQUENCER_VERSION: SequencerVersion = StaticVersion {};
